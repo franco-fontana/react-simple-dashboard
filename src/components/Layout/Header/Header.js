5d04721baec68b72af14db5a0d0ca1121e4f9a3d
Franco Fontana <frfontana@ute.com.uy>
2022-08-12 13:35:28 -0300

    Initial upload

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="text-3xl text-white font-bold bg-blue-800 pt-2 pl-1 pb-2 outline outline-offset-2 outline-8">
+        Header
+      </header>
+    );
+  }
+}
+export default Header;
